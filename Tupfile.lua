34996daf668d845da302472d4599c34e7af38015
Stephen Irons <stephen.irons@clear.net.nz>
2014-01-13 00:09:43 +1300

    Generate scratch files.

--- Tupfile.lua
+++ Tupfile.lua
@@ -9,11 +9,13 @@ local kapfiledir = '/usr/local/share/charts/LINZ/NewZealand/'
 local specs = {}
 local charts = {}
 for _, f in pairs(specfiles) do
-   local spec = f:match('(.+)%.spec')
-   local chart=f:match('(NZ%d+)%-.*%.spec')
-   print(f, spec, chart)
-   charts[chart] = { chart=chart, filename=kapfiledir .. chart .. '.kap' }
-   specs[spec] = { spec=spec, filename=f, chart=chart }
+   local specname = f:match( '(NZ%d+%-.+)%.spec')
+   local chartname = f:match('(NZ%d+)%-.+%.spec')
+   local chart = { name=chartname, filename=kapfiledir .. chartname .. '.kap', specs={} }
+   local spec = { name=specname, filename=f, chart=chart }
+   specs[specname] = spec
+   charts[chartname] = chart
+   print(f, specname, chartname)
 end
 
 
@@ -21,39 +23,60 @@ end
 local tracks = {}
 for _, f in pairs(gpxfiles) do
    local trackname = f:match('gpx/(.+)%.gpx')
-   local t = { filename=f, trackname=trackname }
-   tracks[t] = 1
-   print(f, trackname)
+   local track = { name=trackname, filename=f }
+   tracks[trackname] = track
+--   print(f, trackname)
 end
 
 
 -- Create projected charts for each chart and projection
+local projected_charts = {}
 for c, chart in pairs(charts) do
    for _, projection in pairs(projections) do
-      local projected_chart_filename = chart.chart .. '-' .. projection .. '.tiff'
+      local projected_chart_name = chart.name .. '-' .. projection
+      local projected_chart_filename = projected_chart_name:gsub(':', '_') .. '.tiff'
+      local projected_chart = { name=projected_chart_name, chart=chart, projection=projection, filename=projected_chart_filename }
+      projected_charts[projected_chart_name] = projected_chart
+      print(projected_chart_filename)
+      
       tup.definerule{
-         inputs=nil, outputs={projected_chart_filename},
+         inputs=nil, 
+         outputs={ projected_chart_filename },
          command='gdalwarp -of GTiff -co COMPRESS=LZW  -t_srs ' .. projection .. ' ' .. chart.filename .. ' ' .. projected_chart_filename
       }
    end
 end
 
 
+-- For each scratch chart specification, find the projected charts associated 
+-- with it, and generate a scratch chart
+for s, spec in pairs(specs) do
+   for c, pchart in pairs(projected_charts) do
+      if spec.chart == pchart.chart then
+         local scratch_chart_filename = spec.name .. '-' ..  pchart.projection .. '-scratch.png'
+         print(scratch_chart_filename)
+         tup.definerule{
+            inputs={ spec.filename, pchart.filename },
+            outputs={ scratch_chart_filename },
+            command='./' .. spec.filename .. ' ' .. pchart.filename .. ' ' .. scratch_chart_filename
+         }
+      end
+   end
+end
+
+
 -- Create full-size overlays, one per chart per track
-local overlays = {}
 for c, chart in pairs(charts) do
    for t, track in pairs(tracks) do
---      tup.definerule{inputs{chart.filename, track.filename
+--      tup.definerule{
+--         inputs={chart.filename, track.filename},
+--         outputs={
 --      overlays[ {chart=c, track=t} ] = 1
    end
 end
 
 
 
-scratch_charts = {}
-for s, spec in pairs(specs) do
-   
-end
 -- Create a list of all scratch charts
 -- * one blank one per scratch spec
 -- * one per scratch spec per track
